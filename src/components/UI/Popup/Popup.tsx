e87806dd61c04398d3478ec841a7ad7cf189e2be
Oleksii Vovnenko <kespa71for@gmail.com>
2023-09-22 18:41:47 +0200

    create (custom popup) init files

---
+++ src/components/UI/Popup/Popup.tsx
@@ -0,0 +1,14 @@
+import {FC} from "react";
+
+import styles from "./Popup.module.css"
+const Popup:FC = () => {
+    return (
+        <section
+            className={styles.wrapper}
+        >
+
+        </section>
+    );
+};
+
+export default Popup;
